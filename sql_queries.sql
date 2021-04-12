b8d717e95a0a7e2a88e7e6d50083ba29edc60dc4
renatojmsantos <renatojmsantos@gmail.com>
2021-04-13 00:04:42 +0100

    v0.2

--- sql_queries.sql
+++ sql_queries.sql
@@ -7,6 +7,202 @@ from (
 ) as fi
 group by field;
 
+#vocabulary
+SELECT "public"."annotation"."concept" AS "concept", count(*) AS "count"
+FROM "public"."annotation"
+LEFT JOIN "public"."comment" "Comment - Comment Comment" ON "public"."annotation"."comment_commentid" = "Comment - Comment Comment"."commentid"
+GROUP BY "public"."annotation"."concept"
+ORDER BY "public"."annotation"."concept" ASC
+
+# tentativa...
+SELECT concept, count(distinct comment_commentid)
+from annotation
+where concept LIKE 'Motivation%'
+group by concept
+having count(distinct comment_commentid) > 2
+order by count (distinct comment_commentid) desc
+
+#vocabulary sql
+SELECT annotation.concept, count(distinct comment_commentid) 
+FROM annotation
+LEFT JOIN comment ON annotation.comment_commentid = comment.commentid
+where {{concept}}
+GROUP BY annotation.concept
+ORDER BY annotation.concept ASC
+
+# tentativa...
+select concept, count(*)
+from annotation
+where exists (select 1
+              from annotation
+              where 1=1 [[AND {{concept}} ]][[AND annotation.concept LIKE CONCAT('%',{{concept}},'%')]]
+             )
+group by concept;
+
+# WORKS!!!
+select concept, count(*)
+from annotation
+where exists (select 1
+              from annotation a2
+              where a2.comment_commentid = annotation.comment_commentid and a2.concept = 'Fatigue'
+             )
+group by concept;
+
+#... mb discusse.. WORKS
+select a1.concept, count(*) from annotation a1 where exists (select 1 from annotation where annotation.comment_commentid = a1.comment_commentid and {{concept}} ) group by a1.concept;
+
+#corrigido vocabulario
+SELECT a1.concept, count(distinct comment_commentid) 
+FROM annotation a1
+LEFT JOIN comment ON comment.commentid = a1.comment_commentid
+left join game on game.game_id = a1.game_game_id
+left join video on video.videoid = a1.video_videoid
+where exists (select 1
+              from annotation
+              where {{concept}} and {{polarity}} and {{field}} and {{dateComment}} and {{edition}} and {{platform}} and {{channel}}
+              and annotation.comment_commentid= a1.comment_commentid
+             )
+GROUP BY a1.concept
+ORDER BY a1.concept ASC
+
+# DIMENSIONS
+SELECT a1.field, count(distinct comment_commentid) 
+FROM annotation a1
+JOIN comment ON comment.commentid = a1.comment_commentid
+join game on game.game_id = a1.game_game_id
+join video on video.videoid = a1.video_videoid
+where exists (select 1
+              from annotation
+              where {{concept}} and {{polarity}} and {{field}} and {{dateComment}} and {{edition}} and {{platform}} and {{channel}}
+              and annotation.comment_commentid= a1.comment_commentid
+             )
+GROUP BY a1.field
+ORDER BY a1.field DESC
+
+
+# usability analysis
+select concept, count(distinct comment_commentid)
+from annotation 
+join comment on comment.commentid = annotation.comment_commentid
+join game on game.game_id = annotation.game_game_id
+left join video on video.videoid = annotation.video_videoid
+where field = 'Usability' and {{concept}} and {{polarity}} and {{dateComment}} and {{edition}} and {{platform}} and {{field}} and {{channel}}
+group by concept
+# fix to...
+SELECT a1.concept, count(distinct comment_commentid) 
+FROM annotation a1
+JOIN comment ON comment.commentid = a1.comment_commentid
+join game on game.game_id = a1.game_game_id
+join video on video.videoid = a1.video_videoid
+where exists (select 1
+              from annotation
+              where {{concept}} and {{polarity}} and a1.field = 'Usability' and {{dateComment}} and {{edition}} and {{platform}} and {{channel}}
+              and annotation.comment_commentid= a1.comment_commentid
+             )
+GROUP BY a1.concept
+ORDER BY a1.concept ASC
+
+
+
+# likes dislikes on videos
+SELECT date_trunc('year', video.datevideo) as "Date", max(video.likesvideo) as "Likes", max(video.dislikesvideo) as "Dislikes"
+FROM video
+join annotation on video.videoid = annotation.video_videoid
+join game on game.game_id = annotation.game_game_id
+join comment on comment.commentid = annotation.comment_commentid
+where {{polarity}} and {{field}} and {{concept}} and {{dateVideo}} and {{edition}} and {{platform}} and {{channel}}
+GROUP BY date_trunc('year', video.datevideo)
+ORDER BY date_trunc('year', video.datevideo) ASC
+
+
+SELECT polarity, count(distinct commentid)
+FROM comment
+left join annotation on annotation.comment_commentid = comment.commentid 
+left join game on game.game_id = annotation.game_game_id
+left join video on video.videoid = annotation.video_videoid
+where {{polarity}} and {{field}} and {{concept}} and {{dateComment}} and {{edition}} and {{platform}} and {{channel}}
+GROUP BY comment.polarity
+ORDER BY count(distinct commentid) DESC
+
+#sentiment over time
+SELECT date_trunc('year', comment.datecomment) as "Date", 
+    count(*) filter (where polarity = 'Positive') as positive,
+    count(*) filter (where polarity = 'Neutral') as neutral,
+    count(*) filter (where polarity = 'Negative') as negative,
+FROM comment
+left join annotation on annotation.comment_commentid = comment.commentid 
+left join game on game.game_id = annotation.game_game_id
+left join video on video.videoid = annotation.video_videoid
+GROUP BY date_trunc('year', comment.datecomment)
+ORDER BY date_trunc('year', comment.datecomment) ASC
+
+# likes on comments
+SELECT date_trunc('year', comment.datecomment) as "Date", max(likes) as "Likes"
+FROM comment
+join annotation on comment.commentid = annotation.comment_commentid
+join game on game.game_id = annotation.game_game_id
+join video on video.videoid = annotation.video_videoid
+GROUP BY date_trunc('year', comment.datecomment)
+ORDER BY date_trunc('year', comment.datecomment) ASC
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#as text
+select concept, count(*)
+from annotation
+where exists (select 1
+              from annotation a2
+              where a2.comment_commentid = annotation.comment_commentid and a2.concept = {{searchConcept}}
+             )
+group by concept;
+
+
+#...
+DECLARE @parm NVARCHAR(50);
+SET @parm = (SELECT concept from annotation where {{concept}} LIMIT 1);
+
+
+#work + 1 filtro
+SELECT annotation.concept, count(distinct comment_commentid) 
+FROM annotation
+LEFT JOIN comment ON annotation.comment_commentid = comment.commentid
+left join game on game.game_id = annotation.game_game_id
+left join video on video.videoid = annotation.video_videoid
+where exists (select 1
+              from annotation a2
+              where {{concept}} and {{polarity}} and {{field}} and {{dateComment}} and {{edition}} and {{platform}}
+              and a2.comment_commentid = annotation.comment_commentid [[and a2.concept = {{searchConcept}}]]
+             )
+GROUP BY annotation.concept
+ORDER BY annotation.concept ASC
+
+#...
+select value, count(*)
+from t
+where exists (select 1
+              from t t2
+              where t2.textid = t.textid and t2.value = 'Hedonic'
+             )
+group by value;
+
 
 # u analysis
 select concept, count(*) as count from annotation where field = 'Usability' group by concept
@@ -58,14 +254,27 @@ where {{polarity}} and {{field}} and {{concept}} and {{dateVideo}} and {{edition
 GROUP BY date_trunc('month', video.datevideo)
 ORDER BY date_trunc('month', video.datevideo) ASC
 
+
+
 #game
 SELECT edition, count(distinct game_id)
 FROM game
 left join annotation on game.game_id = annotation.game_game_id
 left join video on video.videoid = annotation.video_videoid
 left join comment on comment.commentid = annotation.comment_commentid
+where {{polarity}} and {{field}} and {{concept}} and {{dateVideo}} and {{edition}} and {{platform}}
 GROUP BY edition
 ORDER BY edition ASC
+
+# channel
+SELECT video.channeltitle, count(distinct videoid) 
+FROM video
+left join annotation on video.videoid = annotation.video_videoid
+left join game on game.game_id = annotation.game_game_id
+left join comment on comment.commentid = annotation.comment_commentid
+where {{polarity}} and {{field}} and {{concept}} and {{dateVideo}} and {{edition}} and {{platform}} and {{channel}}
+GROUP BY video.channeltitle
+ORDER BY video.channeltitle ASC
 
 # comments
 SELECT distinct originaltext, polarity, likes, maincomment
--- vocabulary.py
+++ vocabulary.py
@@ -1,31 +1,44 @@
 
 # ver artigo CHI 13, table 8
 
+# https://ieeexplore.ieee.org/stamp/stamp.jsp?tp=&arnumber=8860063
+"""
+First of all, thank you for your availability to contribute to this validation. 
+This work is performed in the scope of the dissertation of Renato Santos in the context of the Dissertation "Analysing Usability, User Experience, and Perceived Health Impacts of Games for Health based on Users Opinion Mining" of Master in Computer Science in University of Coimbra, under supervision of Professor Paula Alexandra Silva and Professor Joel Perdiz Arrais. 
 
+In this work we intend to validate the WHOQOL-100 questionnaire, focusing on dimensions of physical ability, mental well-being, and social relationships, to annotate theese concepts in user's comments from YouTube videos related with Just Dance game, in order to analyse "perceived" Health impacts.
+
+In this questionnaire, the aim is to understand what annotation would be made by a specialist in at least one of the three areas of action under study. 
+For example, given the comment: "During this quarantine, Just Dance was my buddy. It helped me a lot to stay active and to strengthen friendships with my teammates.", it may annotate concepts like "energy", "self-esteem", "personal relationships".
+
+We would like the honour of having at least 10 comments annoted, but if possible all 32 comments present here.
+
+By proceeding, you agree to the collection of information requested for further analysis in this study.
+"""
 
 dict={	
 		'Usability':{
 			'Memorability': {'memory': 1.0, 'forgot': 0.6, 'remember': 0.8}, 
 			'Learnability': {'learnability': 1.0, 'learn': 1.0, 'intuit': 1.0, 'easier': 1.0,'figur': 0.8,'straightforward': 0.8,'foreword': 0.8, 'practic': 0.6}, 
-			'Efficiency': {'efficiency': 1.0, 'perfect': 0.9, 'effectiveness': 0.6, 'works well': 1.0, 'amazing': 1.0}, 
-			'Errors/Effectiveness': {'errors': 1.0,'effectiveness':1.0, 'fix': 0.8,'problem':0.5, 'incompetent': 0.2, 'broken': 0.7,'camera': 0.7, 'glitch': 0.8,'issu': 0.8,'slow':0.9, 'lag': 0.8,'bug': 0.8, 'inconsist': 0.8},
-			'Satisfaction': {'happy': 1.0, 'fun': 1.0, 'great': 1.0, 'love': 1.0, 'worth': 1.0, 'nice': 1.0, 'best': 1.0, 'recommend': 1.0, 'disappoint': 0.8, 'good': 0.8, 'favorite': 0.8, 'cool': 0.8, 'perfect': 1.0}
+			'Efficiency': {'efficiency': 1.0, 'perfect': 0.9, 'lock':1.0,'slow':1.0,'limit':1.0, 'fast':1.0, 'works well': 1.0}, 
+			'Errors/Effectiveness': {'errors': 1.0,'effectiveness':1.0, 'difficult': 1.0, 'waste':1.0,'easy':1.0,'problem':1.0,'miss':1.0,'wrong':1.0,'fix': 0.8,'problem':0.5, 'incompetent': 0.2, 'broken': 0.7,'camera': 0.7, 'glitch': 0.8,'issu': 0.8, 'lag': 0.8,'bug': 0.8, 'inconsist': 0.8},
+			'Satisfaction': {'happy': 1.0, 'fun': 1.0, 'great': 1.0, 'love': 1.0, 'worth': 1.0, 'best': 1.0, 'recommend': 1.0, 'good': 0.8, 'favorite': 0.8, 'cool': 0.8, 'reliable':1.0,'perfect': 1.0}
 		},
 		'UX':{
+			'Aesthetics and Appeal': {'aesthetics': 1.0, 'interface': 1.0, 'taste': 1.0,'beauty': 1.0,'appreciation': 1.0,'appeal': 1.0, 'graphic':0.9, 'sound':0.9, 'song': 0.9, 'voice':0.9, 'playlist':0.9, 'music':0.9, 'soundtrack':0.9, 'effect':0.8, 'look':0.8, 'color':0.8, 'visual': 0.8, 'detail': 0.6, 'render': 0.5, 'pixel': 0.5},
+			'Affect and Emotion': {'affect': 1.0, 'emotion': 1.0, 'trust': 1.0,'surprise': 1.0,'fear': 1.0,'disgust': 1.0,'frustration': 0.7,'anger': 1.0,'fun': 0.8, 'enjoy': 0.8, 'addict': 0.7, 'workout': 0.7, 'excit': 0.8, 'cute': 0.8, 'nevertheless': 0.8, 'laugh': 0.8, 'annoy': 0.8},
+			'Anticipation': {'anticipation': 1.0, 'expectation': 1.0},
 			'Likeability': {'likeability': 1.0, 'like':0.9}, 
 			'Pleasure': {'pleasure': 1.0, 'fun': 0.7, 'moneybag':0.7, 'enjoy': 0.7, 'love': 0.7, 'entertain': 0.7, 'awesome': 0.8, 'stimulation':0.7, 'felt': 0.7, 'sooth': 0.7, 'adict': 0.7, 'nostalgia': 0.7},
 			'Comfort': {'comfort': 1.0, 'physical': 0.7, 'physical comfort': 0.8, 'workout': 0.8, 'physical needs': 0.9, 'well being':0.8, 'body care':0.3, 'active body':0.9},
 			'Trust': {'trust': 1.0, 'behavior': 0.8},
-			'Anticipation': {'anticipation': 1.0, 'expectation': 1.0},
 			'Overall Usability': {'overall usability': 1.0, 'new version': 0.8, 'upgrade':0.8, 'edition': 0.8, 'previous edition':0.8},
 			'Hedonic': {'hedonic': 1.0, 'fun': 0.8, 'enjoy': 0.8, 'frustrat': 0.8, 'fulfillment': 0.9, 'needs': 0.8, 'pleasure':0.7,'enjoyment':0.7,'frustration':0.7, 'annoy': 0.8, 'entertain': 0.8, 'game': 0.8,'multiplayer': 0.8, 'gaming': 0.8, 'gameplay': 0.8, 'play': 0.8, 'humor': 0.8, 'workout': 0.8, 'nostalgia': 0.6},
 			'Detailed Usability': {'detailed usability': 1.0, 'great': 0.7, 'details': 0.9, 'functions': 0.9, 'satisfaction': 0.7,'usability': 0.7, 'best':0.7, 'problem': 0.7},
 			'User Differences': {'user differences': 1.0,'user group': 0.7,'group': 0.7,'beginners':0.9, 'veterans':0.9,'pro player':0.9, 'amateur':0.9,'professional':0.9,'finalists':0.6, 'professional dancers':0.8,'buyers': 0.7,'target': 0.7,'features': 0.7, 'differences': 0.7, 'if you': 0.6},
-			'Support': {'support':1.0, 'help':0.8, 'wish': 0.7,'software': 0.7,},
+			'Support': {'support':1.0, 'help':0.8, 'how':1.0, 'wish': 0.7,'software': 0.7,},
 			'Impact': {'impact': 1.0, 'pattern': 0.7, 'surprise': 1.0,'fear': 1.0,'change gameplay': 0.9},
-			'Affect and Emotion': {'affect': 1.0, 'emotion': 1.0, 'trust': 1.0,'surprise': 1.0,'fear': 1.0,'disgust': 1.0,'frustration': 0.7,'anger': 1.0,'fun': 0.8, 'enjoy': 0.8, 'addict': 0.7, 'workout': 0.7, 'excit': 0.8, 'cute': 0.8, 'nevertheless': 0.8, 'laugh': 0.8, 'annoy': 0.8},
 			'Enjoyment and Fun': {'joy':0.9, 'enjoyment': 1.0, 'hedonic': 1.0,'emotion': 1.0,'affect': 1.0,'fun': 1.0, 'younger': 0.7, 'entertain': 0.9},
-			'Aesthetics and Appeal': {'aesthetics': 1.0, 'taste': 1.0,'beauty': 1.0,'appreciation': 1.0,'appeal': 1.0, 'graphic':0.9, 'sound':0.9, 'song': 0.9, 'voice':0.9, 'playlist':0.9, 'music':0.9, 'soundtrack':0.9, 'effect':0.8, 'look':0.8, 'color':0.8, 'visual': 0.8, 'detail': 0.6, 'render': 0.5, 'pixel': 0.5},
 			'Engagement': {'engagement': 1.0, 'challeng': 0.9, 'flow': 1.0,'skills': 1.0,'needs': 1.0,'forget': 1.0,'engaged': 1.0,'addict': 0.9, 'addition': 1.0, 'replay':0.7, 'nonstop': 0.9, 'interest':0.7},
 			'Motivation': {'motivation': 1.0, 'task': 1.0, 'joy': 0.8},
 			'Enchantment': {'enchantment': 1.0, 'concentration': 1.0,'attention': 1.0,'liveliness': 1.0,'fullness': 1.0,'pleasure': 1.0,'disorientation': 1.0,'experience': 1.0},
