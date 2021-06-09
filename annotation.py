6645baf82155a77fdb7a9fd44a795af2044ee372
renatojmsantos <renatojmsantos@gmail.com>
2021-06-09 16:47:20 +0100

    fix

--- annotation.py
+++ annotation.py
@@ -651,176 +651,11 @@ def getSentiment(t):
 		print("sentiment — ",e)
 
 
-def updateEditionAndPlataform(game_id, title, descript):
-	try:
-		title = re.sub('&quot;+','',title)
-		title = title.lower() 
-
-		# substituir JD por Just Dance .... no titulo do video ....
-		title = re.sub('jd','just dance',title)
-		title = re.sub('justdance','just dance',title)
-		title = re.sub('wiiu','wii u',title)
-		#title = re.sub('ps2','PlayStation 2',title)
-		title = re.sub('ps3','PlayStation 3',title)
-		title = re.sub('ps4','PlayStation 4',title)
-		title = re.sub('ps5','PlayStation 5',title)
-		#title = re.sub('playstation2','PlayStation 2',title)
-		title = re.sub('playstation3','PlayStation 3',title)
-		title = re.sub('playstation4','PlayStation 4',title)
-		title = re.sub('playstation5','PlayStation 5',title)
-		title = re.sub('x360','Xbox 360',title)
-		title = re.sub('xbox sx','Xbox Series X',title)
-		title = re.sub('xbox ss','Xbox Series S',title)
-		title = re.sub('xbox360','Xbox 360',title)
-		title = re.sub('xboxone','Xbox One',title)
-		title = re.sub('switch','Nintendo Switch',title)
-		title = re.sub('nintendoswitch','Nintendo Switch',title)
-		title = re.sub('nintendo','Nintendo Switch',title)
-		title = re.sub('windows','Microsoft Windows',title)
-		title = re.sub('pc','Microsoft Windows',title)
-
-		titleWords = word_tokenize(title.strip()) 
-		title = " ".join(titleWords)
-		title = title.lower()
-
-		#descript = description[row]
-		descriptWords = word_tokenize(descript.strip()) 
-		descript = " ".join(descriptWords)
-
-		descript = " ".join(descript.strip().split())
-		descript = re.sub(r"[\W\s]"," ",descript)
-		descript = re.sub("\n","",descript)
-
-		descript = descript.lower()
-		descript = re.sub('jd','just dance',descript)
-		descript = re.sub('justdance','just dance',descript)
-		descript = re.sub('wiiu','wii u',descript)
-		#descript = re.sub('ps2','PlayStation 2',descript)
-		descript = re.sub('ps3','PlayStation 3',descript)
-		descript = re.sub('ps4','PlayStation 4',descript)
-		descript = re.sub('ps5','PlayStation 5',descript)
-		descript = re.sub('playstation3','PlayStation 3',descript)
-		descript = re.sub('playstation4','PlayStation 4',descript)
-		descript = re.sub('playstation5','PlayStation 5',descript)
-		descript = re.sub('x360','Xbox 360',descript)
-		descript = re.sub('xbox sx','Xbox Series X',descript)
-		descript = re.sub('xbox ss','Xbox Series S',descript)
-		descript = re.sub('xbox360','Xbox 360',descript)
-		descript = re.sub('xboxone','Xbox One',descript)
-		descript = re.sub('nintendoswitch','Nintendo Switch',descript)
-		descript = re.sub('switch','Nintendo Switch',descript)
-		descript = re.sub('nintendo','Nintendo Switch',descript)
-		descript = re.sub('windows','Microsoft Windows',descript)
-		descript = re.sub('pc','Microsoft Windows',descript)
-		descript = descript.lower()
-
-		platforms = ['Wii', 'Wii U', 'PlayStation 3', 'PlayStation 4', 'PlayStation 5', 'Xbox 360', 'Xbox One', 'Xbox Series X', 'Xbox Series S','iOS', 'Android', 'Nintendo Switch', 'Microsoft Windows', 'Stadia']
-		# detetar plataforma no titulo do video e na descrição ...
-		platform = ""
-		for p in platforms:
-			c = p.lower()
-			if(c in title.lower()):
-				#print(p)
-				platform = p
-				if (platform == "Wii"):
-					continue
-				elif (platform == "Xbox Series X"):
-					continue
-				else:
-					break
-				#break
-			elif(c in descript.strip().lower()):
-				platform = p
-				if (platform == "Wii"):
-					continue
-				elif (platform == "Xbox Series X"):
-					continue
-				else:
-					break
-
-		if(platform==""):
-			platform="Unknown"
-
-		#print(title)
-		#https://en.wikipedia.org/wiki/Just_Dance_(video_game_series)
-		games = ['Just Dance 2', 'Just Dance 3', 'Just Dance 4', 'Just Dance 2014', 'Just Dance 2015', 'Just Dance 2016', 'Just Dance 2017', 'Just Dance 2018', 'Just Dance 2019', 'Just Dance 2020', 'Just Dance 2021',
-				'Just Dance Wii', 'Just Dance Wii 2', 'Just Dance Wii U', 'Yo-kai Watch Dance: Just Dance Special Version',
-				'Just Dance Kids', 'Just Dance Kids 2', 'Just Dance Kids 2014',
-				'Just Dance: Disney Party', 'Just Dance: Disney Party 2',
-				'Just Dance: Greatest Hits',
-				'Just Dance: Summer Party', 'Just Dance Now', 'Just Dance Unlimited']
-		# Just Dance é o ultimo jogo a ser inserido... RISCO neste!!! pode nao ser o 1.º JD.... pq no titulo podem nao especificar qual é a versao
-		# quem nao quiser saber de qual é a edicao, simplesmente nao aplica o filtro, e vê tudo.
-		
-		# detetar o nome do jogo no titulo do video ...
-		edition=""
-		serie=""
-		
-		for game in games:
-			serie = game.lower()
-			if (serie == 'yo-kai watch dance: just dance special version'):
-				serie = 'yo-kai watch dance just dance special version'
-
-			elif(serie == 'just dance: disney party'):
-				serie = 'just dance disney party'
-
-			elif(serie == 'just dance: disney party 2'):
-				serie = 'just dance disney party 2'
-
-			elif(serie == 'just dance: greatest hits'):
-				serie = 'just dance greatest hits'
-
-			elif(serie == 'just dance: summer party'):
-				serie = 'just dance summer party'
-
-			if(serie in title.lower()):
-				edition=game
-				#print(edition)
-				if (edition == "Just Dance 2"):
-					continue
-				if (edition == "Just Dance Wii"):
-					continue
-				if (edition == "Just Dance Kids"):
-					continue
-				if (edition == "Just Dance Kids 2"):
-					continue
-				if (edition == "Just Dance: Disney Party"):
-					continue
-				else:
-					break
-			elif(serie in descript.lower()):
-				edition=game
-				if (edition == "Just Dance 2"):
-					continue
-				if (edition == "Just Dance Wii"):
-					continue
-				if (edition == "Just Dance Kids"):
-					continue
-				if (edition == "Just Dance Kids 2"):
-					continue
-				if (edition == "Just Dance: Disney Party"):
-					continue
-				else:
-					break
-		if(edition == ""): # PROBLEM 2018, 2019 ???
-			serie = "Just Dance"
-			if(serie.lower() in title.lower()):
-				edition = "Just Dance"
-
-		print("UPDATE -> ",edition," -> ", platform)
-
-		updateGame(str(game_id), str(edition), str(platform))
-		
-	except Exception as e:
-		print("get game and console ->", e)
-
-
-
 
 def getEditionAndPlataform(game_id, title, descript):
 	try:
 		title = re.sub('&quot;+','',title)
-		title = title.lower() 
+		title = title.lower().strip()
 
 		# substituir JD por Just Dance .... no titulo do video ....
 		title = re.sub('jd','just dance',title)
@@ -845,9 +680,16 @@ def getEditionAndPlataform(game_id, title, descript):
 		title = re.sub('windows','Microsoft Windows',title)
 		title = re.sub('pc','Microsoft Windows',title)
 		title = re.sub('iphone','iOS',title)
-		
-		titleWords = word_tokenize(title.strip()) 
-		title = " ".join(titleWords)
+
+		#titleWords = word_tokenize(title.strip()) 
+		#title = " ".join(titleWords)
+
+		title = re.sub('yo-kai watch dance just dance special version','yo-kai watch dance: just dance special version', title)
+		title = re.sub('just dance disney party','just dance: disney party',title)
+		title = re.sub('just dance disney party 2','just dance: disney party 2',title)
+		title = re.sub('just dance greatest hits','just dance: greatest hits',title)
+		title = re.sub('just dance summer party','just dance: summer party',title)
+
 		title = title.lower()
 
 		#descript = description[row]
@@ -900,20 +742,6 @@ def getEditionAndPlataform(game_id, title, descript):
 		
 		for game in games:
 			serie = game.lower()
-			if (serie == 'yo-kai watch dance: just dance special version'):
-				serie = 'yo-kai watch dance just dance special version'
-
-			elif(serie == 'just dance: disney party'):
-				serie = 'just dance disney party'
-
-			elif(serie == 'just dance: disney party 2'):
-				serie = 'just dance disney party 2'
-
-			elif(serie == 'just dance: greatest hits'):
-				serie = 'just dance greatest hits'
-
-			elif(serie == 'just dance: summer party'):
-				serie = 'just dance summer party'
 
 			if(serie in title.lower()):
 				edition=game
@@ -928,8 +756,7 @@ def getEditionAndPlataform(game_id, title, descript):
 					continue
 				if (edition == "Just Dance: Disney Party"):
 					continue
-				else:
-					break
+				
 			"""
 			elif(serie in descript.lower()):
 				edition=game
@@ -967,10 +794,9 @@ def getEditionAndPlataform(game_id, title, descript):
 				platform = p
 				if (platform == "Wii"):
 					continue
-				elif (platform == "Xbox Series X"):
+				if (platform == "Xbox Series X"):
 					continue
-				else:
-					break
+				
 				#break
 			"""
 			elif(c in descript.strip().lower()):
