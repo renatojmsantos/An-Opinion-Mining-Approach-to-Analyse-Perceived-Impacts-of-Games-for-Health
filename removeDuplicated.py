136d23a073039253549052ccaebc4ee7e4f93d5b
renatojmsantos <renatojmsantos@gmail.com>
2021-06-16 22:38:33 +0100

    fix

--- removeDuplicated.py
+++ removeDuplicated.py
@@ -89,7 +89,7 @@ def getComments():
 		#conn.autocommit = True
 		cur = conn.cursor()
 		#processedtext, originaltext
-		query = "SELECT originaltext,commentid, (array_length(regexp_split_to_array(originaltext, '\s+'),1)) as pals FROM comment join annotation on annotation.comment_commentid = comment.commentid where (array_length(regexp_split_to_array(originaltext, '\s+'),1)) > 0 group by originaltext, commentid order by pals"
+		query = "SELECT originaltext,commentid, (array_length(regexp_split_to_array(originaltext, '\s+'),1)) as pals FROM comment join annotation on annotation.comment_commentid = comment.commentid where (array_length(regexp_split_to_array(originaltext, '\s+'),1)) > 5 group by originaltext, commentid order by pals"
 		#query = "SELECT originaltext,commentid, (array_length(regexp_split_to_array(originaltext, '\s+'),1)) as pals FROM comment join annotation on annotation.comment_commentid = comment.commentid group by originaltext, commentid order by pals"
 		#print(query)
 		cur.execute(query)
@@ -160,7 +160,7 @@ def deleteNonEnglish():
 		comments = getComments()
 		for c in comments:
 			original = c[0]
-			print("\n",original)
+			#print("\n",original)
 			cid = c[1]
 			try:
 				if(isEnglish(str(original.lower())) is False):
