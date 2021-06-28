d4a7e3d2ba580ce02af6790cd8a8a4aa453a775b
renatojmsantos <renatojmsantos@gmail.com>
2021-06-29 00:52:13 +0100

    fix

--- removeDuplicated.py
+++ removeDuplicated.py
@@ -324,7 +324,7 @@ def getCommentsSentiment():
 		cur = conn.cursor()
 		#query = "SELECT commentid from comment where polarity = 'Neutral' order by commentid limit 25000"
 		
-		query = "SELECT annotationid from comment join annotation on annotation.comment_commentid = comment.commentid where polarity = 'Neutral' and concept = 'Frustration'"
+		query = "SELECT annotationid from comment join annotation on annotation.comment_commentid = comment.commentid where polarity = 'Neutral' and concept = 'Likeability'"
 
 		cur.execute(query)
 		idBack = cur.fetchall()
