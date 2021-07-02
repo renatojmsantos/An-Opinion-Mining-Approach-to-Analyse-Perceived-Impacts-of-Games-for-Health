4eb7b83b31dfc5be88b768386da2fe5c58f6b711
renatojmsantos <renatojmsantos@gmail.com>
2021-07-02 14:15:07 +0100

    fix

--- runSystem.sh
+++ runSystem.sh
@@ -1,14 +1,3 @@
-
-
-#python3 YouTube_Extractor.py 2009-09-17 7 2 True 'random'
-#python3 YouTube_Extractor.py 2009-09-17 4200 2 True 'random'
-
-
-#while [ $(date "+%H-%M") -lt $(date "+14:02")]; do
-#    echo "test"
-#    sleep 1
-#done
-
 #!/bin/bash
 
 runtime="5 minute"
