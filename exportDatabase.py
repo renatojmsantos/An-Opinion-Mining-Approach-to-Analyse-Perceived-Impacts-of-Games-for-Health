e2412cef36c7deba0502eddbef800cfa43772a43
renatojmsantos <renatojmsantos@gmail.com>
2021-07-09 23:21:25 +0100

    fix

--- exportDatabase.py
+++ exportDatabase.py
@@ -34,54 +34,55 @@ def getFeatures():
 
 
 def export():
-	try:
+	
 		#f = open('../justDance','w')
 		#write = csv.writer(f)
 
-		header = ['annotationID','dimension','concept','commentID', 'originalText','expandedText','sentiment','likes','dateComment','mainComment',
-				'gameID','edition','platform','channelID','channelTitle','videoID','videoTitle','dateVideo','viewsVideo','likesVideo','dislikesVideo','totalCommentsVideo','descriptionVideo']
+	header = ['annotationID','dimension','concept','commentID', 'originalText','expandedText','sentiment','likes','dateComment','mainComment',
+			'gameID','edition','platform','channelID','channelTitle','videoID','videoTitle','dateVideo','viewsVideo','likesVideo','dislikesVideo','totalCommentsVideo','descriptionVideo']
 
-		
-		with open('../justDance.csv', 'w', encoding='UTF8', newline='') as f:
-			writer = csv.writer(f)
-			writer.writerow(header)
+	print("yes0")
+	with open('../justDance.csv', 'w', encoding='UTF8', newline='') as f:
+		print("yes1")
+		writer = csv.writer(f)
+		print("yes2")
+		writer.writerow(header)
 
-			cols = getFeatures()
-			print("yes")
-			for col in cols:
-				data = []
-				commentID = col[0]
-				originalText = col[1]
-				expandedText = col[2]
-				sentiment = col[3]
-				likes = col[4]
-				dateComment = col[5]
-				mainComment = col[6]
-				annotationID = col[7]
-				dimension = col[8]
-				concept = col[9]
-				gameID = col[13]
-				edition = col[14]
-				platform = col[15]
-				channelID = col[16]
-				channelTitle = col[17]
-				videoID = col[18]
-				videoTitle = col[19]
-				dateVideo = col[20]
-				viewsVideo = col[21]
-				likesVideo = col[22]
-				dislikesVideo = col[23]
-				totalCommentsVideo = col[24]
-				descriptionVideo = col[25]
+		cols = getFeatures()
+		print("yes")
+		for col in cols:
+			data = []
+			commentID = col[0]
+			originalText = col[1]
+			expandedText = col[2]
+			sentiment = col[3]
+			likes = col[4]
+			dateComment = col[5]
+			mainComment = col[6]
+			annotationID = col[7]
+			dimension = col[8]
+			concept = col[9]
+			gameID = col[13]
+			edition = col[14]
+			platform = col[15]
+			channelID = col[16]
+			channelTitle = col[17]
+			videoID = col[18]
+			videoTitle = col[19]
+			dateVideo = col[20]
+			viewsVideo = col[21]
+			likesVideo = col[22]
+			dislikesVideo = col[23]
+			totalCommentsVideo = col[24]
+			descriptionVideo = col[25]
 
-				#print(commentID,originalText)
-				data.extend([annotationID,dimension,concept,commentID,originalText,expandedText,sentiment,likes,dateComment,mainComment,gameID,edition,platform,channelID,channelTitle,videoID,videoTitle,dateVideo,viewsVideo, likesVideo,dislikesVideo,totalCommentsVideo,descriptionVideo])
-				#print(data)
-				writer.writerow(data)
-				time.sleep(0.5)
+			#print(commentID,originalText)
+			data.extend([annotationID,dimension,concept,commentID,originalText,expandedText,sentiment,likes,dateComment,mainComment,gameID,edition,platform,channelID,channelTitle,videoID,videoTitle,dateVideo,viewsVideo, likesVideo,dislikesVideo,totalCommentsVideo,descriptionVideo])
+			#print(data)
+			writer.writerow(data)
+			time.sleep(0.5)
 			
-	except Exception as e:
-		print(e)
+	
 
 export()
 
