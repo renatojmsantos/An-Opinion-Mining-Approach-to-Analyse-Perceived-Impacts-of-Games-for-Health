2a897ee0dcb8a16f2a44c7da0291c83bccc20210
renatojmsantos <renatojmsantos@gmail.com>
2021-07-10 00:03:44 +0100

    fix

--- exportDatabase.py
+++ exportDatabase.py
@@ -20,11 +20,59 @@ def getFeatures():
 
 		query = "SELECT annotationid,field,concept,comment_commentid,originaltext, processedtext,polarity,likes, dateComment, mainComment, game_game_id, edition, platform, channelID, channelTitle, video_videoid, videoTitle, dateVideo, viewsVideo, likesVideo,dislikesvideo, totalcommentsvideo,description FROM annotation join comment on comment.commentid = annotation.comment_commentid join game on game.game_id = annotation.game_game_id join video on video.videoid = annotation.video_videoid"
 
-		cur.itersize = 10000
+		#cur.itersize = 10000
 		#print(query)
 		cur.execute(query)
-		idBack = cur.fetchall() # TUPLO
-		
+		#idBack = cur.fetchall() # TUPLO
+		row = cur.fetchone()
+		print("yes0")
+		with open('../justDance.csv', 'w', encoding='utf-8', newline='') as f:
+
+			header = ['annotationID','dimension','concept','commentID', 'originalText','expandedText','sentiment','likes','dateComment','mainComment',
+				'gameID','edition','platform','channelID','channelTitle','videoID','videoTitle','dateVideo','viewsVideo','likesVideo','dislikesVideo','totalCommentsVideo','descriptionVideo']
+
+			print("yes1")
+			writer = csv.writer(f)
+			print("yes2")
+			writer.writerow(header)
+			print("yes3")
+			while row:	
+				#print(row)
+				col = row	
+				#print(col[0])
+				data = []
+				annotationID = col[0]
+				dimension = col[1]
+				concept = col[2]
+				commentID = col[3]
+				originalText = col[4]
+				expandedText = col[5]
+				sentiment = col[6]
+				likes = col[7]
+				dateComment = col[8]
+				mainComment = col[9]
+				gameID = col[10]
+				edition = col[11]
+				platform = col[12]
+				channelID = col[13]
+				channelTitle = col[14]
+				videoID = col[15]
+				videoTitle = col[16]
+				dateVideo = col[17]
+				viewsVideo = col[18]
+				likesVideo = col[19]
+				dislikesVideo = col[20]
+				totalCommentsVideo = col[21]
+				descriptionVideo = col[22]
+
+				#print(commentID,originalText)
+				data.extend([annotationID,dimension,concept,commentID,originalText,expandedText,sentiment,likes,dateComment,mainComment,gameID,edition,platform,channelID,channelTitle,videoID,videoTitle,dateVideo,viewsVideo, likesVideo,dislikesVideo,totalCommentsVideo,descriptionVideo])
+				#print(data)
+				writer.writerow(data)
+				#time.sleep(0.5)
+
+				row = cur.fetchone()
+
 		cur.close()
 		#return idBack
 	except (Exception, psycopg2.DatabaseError) as error:
@@ -35,6 +83,7 @@ def getFeatures():
 			conn.close()
 	return idBack# is not None #idBack
 
+getFeatures()
 
 def export():
 	
@@ -88,5 +137,5 @@ def export():
 			
 
 
-export()
+#export()
 
